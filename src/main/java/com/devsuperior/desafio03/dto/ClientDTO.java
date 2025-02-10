b1bcd6e77d00918a9f98b1d62cc87a73392ad84d
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-02-10 17:34:05 -0300

    GET client by Id

--- src/main/java/com/devsuperior/desafio03/dto/ClientDTO.java
+++ src/main/java/com/devsuperior/desafio03/dto/ClientDTO.java
@@ -1,9 +1,12 @@
 package com.devsuperior.desafio03.dto;
 
+import com.devsuperior.desafio03.entities.Client;
+
 import java.time.LocalDate;
 
 public class ClientDTO {
 
+    private Long id;
     private String name;
     private String cpf;
     private Double income;
@@ -13,12 +16,26 @@ public class ClientDTO {
     public ClientDTO() {
     }
 
-    public ClientDTO(String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
         this.name = name;
         this.cpf = cpf;
         this.income = income;
         this.birthDate = birthDate;
         this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        id = entity.getId();
+        name = entity.getName();
+        cpf = entity.getCpf();
+        income = entity.getIncome();
+        birthDate = entity.getBirthDate();
+        children = entity.getChildren();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
---
+++ src/main/java/com/devsuperior/desafio03/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.desafio03.services;
+
+import com.devsuperior.desafio03.dto.ClientDTO;
+import com.devsuperior.desafio03.entities.Client;
+import com.devsuperior.desafio03.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+}
