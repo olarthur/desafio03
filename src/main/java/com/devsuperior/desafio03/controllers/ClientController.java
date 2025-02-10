84a1e40851b6447dee738785156d951b3f39b1d4
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-02-10 17:44:11 -0300

    GET clients paged

--- src/main/java/com/devsuperior/desafio03/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio03/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.desafio03.controllers;
 import com.devsuperior.desafio03.dto.ClientDTO;
 import com.devsuperior.desafio03.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +20,10 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ClientDTO findByID(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/desafio03/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio03/services/ClientService.java
@@ -4,6 +4,8 @@ import com.devsuperior.desafio03.dto.ClientDTO;
 import com.devsuperior.desafio03.entities.Client;
 import com.devsuperior.desafio03.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(ClientDTO::new);
     }
 }
