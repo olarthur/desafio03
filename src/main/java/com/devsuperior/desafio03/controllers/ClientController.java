9f31162d8da588a531a033d37d740796305e3d6b
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-02-10 18:07:44 -0300

    POST - Insert new Client

--- src/main/java/com/devsuperior/desafio03/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio03/controllers/ClientController.java
@@ -5,10 +5,7 @@ import com.devsuperior.desafio03.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -25,5 +22,10 @@ public class ClientController {
     @GetMapping
     public Page<ClientDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ClientDTO insert(@RequestBody ClientDTO dto) {
+        return service.insert(dto);
     }
 }
